01174af82f56c1f633534b53d8f48a0aefbd75ec
polcaparross <polcaparrosescudero@gmail.com>
2024-06-03 23:15:35 +0200

    Backend Done

--- src/exam.js
+++ src/exam.js
@@ -1,4 +1,3 @@
-
 //== Backend =================================================================
 
 const convolutedCryptoProvider = function(seed) {
@@ -12,7 +11,88 @@ const convolutedCryptoProvider = function(seed) {
 
 // TODO
 
-// ... Catcha ...
+const Catcha = function(challenge){
+
+  let images = new Array(9)
+
+  let generated = convolutedCryptoProvider(challenge)
+
+  let calcArray = () => {
+    return new Promise((resolve, reject) => {
+      generated.then((res) => {
+        images[0] = res.boolean;
+
+        res.next().then((res2) => {
+          images[1] = res2.boolean;
+
+          res2.next().then((res3) => {
+            images[2] = res3.boolean;
+
+            res3.next().then((res4) => {
+              images[3] = res4.boolean;
+
+              res4.next().then((res5) => {
+                images[4] = res5.boolean;
+
+                res5.next().then((res6) => {
+                  images[5] = res6.boolean;
+
+                  res6.next().then((res7) => {
+                    images[6] = res7.boolean;
+
+                    res7.next().then((res8) => {
+                      images[7] = res8.boolean;
+
+                      res8.next().then((res9) => {
+                        images[8] = res9.boolean;
+                        resolve();
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  };
+
+  this.getImages = () =>{
+    return new Promise((resolve, reject) => {
+      calcArray().then(() => {
+        let catsIndex = 0;
+        let bikesIndex = 0;
+        let finalImages = images.map((el) => {
+          if (el == true) {
+            catsIndex++;
+            return `/public/img/cat${catsIndex}.png`;
+          } else {
+            bikesIndex++;
+            return `/public/img/bike${bikesIndex}.png`;
+          }
+        });
+        resolve(finalImages);
+      });
+    });
+  }
+
+  this.checkAnswer = (selected) => {
+    return new Promise((resolve, reject) => {
+      calcArray().then(() => {
+        let result = true;
+        if (images.length == selected.length) {
+          images.forEach((el, index) => {
+            if (el != selected[index]) {
+              result = false;
+            }
+          });
+          resolve(result);
+        }
+      });
+    });
+  };
+}
 
 
 
@@ -48,21 +128,20 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/challenge', function(req, res) {
 
-  if (typeof Catcha != "undefined") {
-    const challenge = Math.floor(Math.random() * 13);
-    const catcha = new Catcha(challenge);
+  const challenge = Math.floor(Math.random() * 13);
+  const catcha = new Catcha(challenge);
 
-    catcha.getImages().then(images => {
-      res.json({ challenge: challenge, images: images });
-    });
-  } else {
+  catcha.getImages().then(images => {
+     res.json({ challenge: challenge, images: images });
+  });
+  /* } else {
     // Fallback
     res.json({ challenge: 'TODO', images:
       ['img/cat1.png','img/cat1.png','img/bike1.png',
       'img/bike1.png','img/bike1.png','img/bike1.png',
       'img/bike1.png','img/bike1.png','img/bike1.png']
     });
-  }
+  } */
 });
 
 app.post('/response/:challenge', function(req, res) {
