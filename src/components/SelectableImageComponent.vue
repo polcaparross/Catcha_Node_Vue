e23ae7df4fb39b3d0301162a25ef474aa9dd2c5c
polcaparross <polcaparrosescudero@gmail.com>
2024-06-03 12:53:52 +0200

    Frontend Done

--- src/components/SelectableImageComponent.vue
+++ src/components/SelectableImageComponent.vue
@@ -1,11 +1,39 @@
 <script>
 export default{
-  
+    props:['src', 'modelValue'],
+    emits:['update:modelValue'],  
+    data(){
+        return{
+        border: 'none'
+
+        }
+    },
+    watch:{
+        border(newVal){
+            if(newVal == 'border: 4px red solid'){
+                this.$emit('update:modelValue', true)
+            } else {
+                this.$emit('update:modelValue', false)
+            }
+        }
+
+    },
+    computed:{
+        handleClick(){
+            if(this.border == 'none'){
+            
+                this.border = `border: 4px red solid`
+            }else{
+                this.border = 'none'
+            }
+            
+        }
+    }
 }
 </script>
 
 <template>
-
+    <img :src="src" :style="border" @click="handleClick" >
 </template>
 
 <style>
